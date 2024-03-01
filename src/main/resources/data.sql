37bb3b65dccceeaa8108446d20a76999e2189009
Sohael El Makkaoui <r101008@student.thomasmore.be>
2024-03-01 21:13:57 +0100

    data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,3 +13,13 @@ VALUES (4, 'Sporthal Linkeroever', 'Blancefloerlaan 183, 2050 Antwerpen ', 50, '
 
 INSERT INTO zaal (id, Zaalnaam, locatie, capaciteit, beschrijving,image, tarief)
 VALUES (5, 'Sporthal Parkloods', 'Damplein 36, 2060 Antwerpen ', 50, 'Je kan in deze sporthal terecht voor zaalvoetbal, basketbal, badminton, vechtsporten, gymnastiek en volleybal.','sporthalluchtball.jpg ',12 );
+
+INSERT INTO zaal (id, Zaalnaam, locatie, capaciteit, beschrijving,image, tarief)
+VALUES (6, 'Sporthal Arena', 'Arenaplein 1, 2100 Antwerpen', 50, 'In deze sporthal kan u terecht voor basketbal en volleybal.','sporthalluchtball.jpg ',18 );
+
+INSERT INTO zaal (id, Zaalnaam, locatie, capaciteit, beschrijving,image, tarief)
+VALUES (7, 'Sporthal De Rode Loop', 'Rodeloopstraat 2, 2170 Antwerpen ', 50, 'In sporthal De Rode Loop kan je terecht voor basketbal, handbal, korfbal, tennis, volleybal en zaalvoetbal. In de vechtsportzaal kan je onder meer judo, karate en jiu jitsu beoefenen','sporthalluchtball.jpg ',24 );
+
+
+INSERT INTO zaal (id, Zaalnaam, locatie, capaciteit, beschrijving,image, tarief)
+VALUES (8, 'Sporthal Het Rooi in Berchem', 'Rodeloopstraat 2, 2170 Antwerpen ', 50, 'In sporthal Het Rooi kunt u terecht voor badminton, basketbal, handbal, korfbal, tennis, volleybal, zaalvoetbal in de grote zaal. Er is ook een kleinere zaal waar u kan turnen, tafeltennissen en schermen.','sporthalluchtball.jpg ',22 );
